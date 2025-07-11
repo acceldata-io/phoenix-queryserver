2877d36706a53079885ff1bac2cb71ca60b22109
Prabhjyot Singh <prabhjyotsingh@gmail.com>
2025-07-10 21:42:38 -0400

    ODP-4648: Phoenix - Unable to start the service CNF exception - fix regex to pick correct jar instead of javadoc/sources/tests

--- bin/phoenix_queryserver_utils.py
+++ bin/phoenix_queryserver_utils.py
@@ -82,7 +82,7 @@ def setPath():
     PHOENIX_CLIENT_JAR_PATTERN = "phoenix-client-*.jar"
     OLD_PHOENIX_CLIENT_JAR_PATTERN = "phoenix-*[!n]-client.jar"
     PHOENIX_THIN_CLIENT_JAR_PATTERN = "phoenix-queryserver-client-*.jar"
-    PHOENIX_QUERYSERVER_JAR_PATTERN = "phoenix-queryserver-[!c]*.jar"
+    PHOENIX_QUERYSERVER_JAR_PATTERN = "phoenix-queryserver-[0-9.]*[0-9].jar"
     PHOENIX_LOADBALANCER_JAR_PATTERN = "load-balancer-*[!t][!e][!s][!t][!s].jar"
     SQLLINE_WITH_DEPS_PATTERN = "sqlline-*-jar-with-dependencies.jar"
     SLF4J_BACKEND_JAR_PATTERN = "log4j-slf4j*.jar"
